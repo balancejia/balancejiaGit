6a25fb2dbdbed1837d108fd1ba7f61e33851156b
balancejia <balancejia@balancejia-PC>
2015-04-17 10:41:05 +0800

    接口开发完成

--- fireWater-api/src/main/java/com/thx/fireWater/interfaces/dto/MapSite.java
+++ fireWater-api/src/main/java/com/thx/fireWater/interfaces/dto/MapSite.java
@@ -19,17 +19,6 @@ public class MapSite {
 	protected String docURL;// 文档请求地址
 	protected int docNUM;// 非图片资源个数，资源id范围：0~docNUM
 
-	protected String field0;
-	protected String field1;
-	protected String field2;
-	protected String field3;
-	protected String field4;
-	protected String field5;
-	protected String field6;
-	protected String field7;
-	protected String field8;
-	protected String field9;
-
 	public String getSiteId() {
 		return siteId;
 	}
@@ -92,86 +81,6 @@ public class MapSite {
 
 	public void setDocNUM(int docNUM) {
 		this.docNUM = docNUM;
-	}
-
-	public String getField0() {
-		return field0;
-	}
-
-	public void setField0(String field0) {
-		this.field0 = field0;
-	}
-
-	public String getField1() {
-		return field1;
-	}
-
-	public void setField1(String field1) {
-		this.field1 = field1;
-	}
-
-	public String getField2() {
-		return field2;
-	}
-
-	public void setField2(String field2) {
-		this.field2 = field2;
-	}
-
-	public String getField3() {
-		return field3;
-	}
-
-	public void setField3(String field3) {
-		this.field3 = field3;
-	}
-
-	public String getField4() {
-		return field4;
-	}
-
-	public void setField4(String field4) {
-		this.field4 = field4;
-	}
-
-	public String getField5() {
-		return field5;
-	}
-
-	public void setField5(String field5) {
-		this.field5 = field5;
-	}
-
-	public String getField6() {
-		return field6;
-	}
-
-	public void setField6(String field6) {
-		this.field6 = field6;
-	}
-
-	public String getField7() {
-		return field7;
-	}
-
-	public void setField7(String field7) {
-		this.field7 = field7;
-	}
-
-	public String getField8() {
-		return field8;
-	}
-
-	public void setField8(String field8) {
-		this.field8 = field8;
-	}
-
-	public String getField9() {
-		return field9;
-	}
-
-	public void setField9(String field9) {
-		this.field9 = field9;
 	}
 
 }
--- fireWater-server/src/main/java/com/thx/fireWater/app/impl/SiteManagerImpl.java
+++ fireWater-server/src/main/java/com/thx/fireWater/app/impl/SiteManagerImpl.java
@@ -7,29 +7,38 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.thx.common.spring.BeanFactory;
 import com.thx.fireWater.interfaces.dto.WaterSite;
 import com.thx.fireWater.interfaces.jsonrpc.SiteManager;
+import com.thx.fireWater.model.MapSite;
+import com.thx.fireWater.service.ISiteManager;
 
 /**
- * @author balancejia
- * 站点服务
+ * @author balancejia 站点服务
  *
  */
 public class SiteManagerImpl implements SiteManager {
 
+	public static String TYPE_WATER = "w";
+	public static String TYPE_FIRE = "f";
+	public static String TYPE_ARMY = "a";
+
+	private static String IMGURL = "resource/img.action";
+	private static String DOCURL = "resource/doc.action";
+
 	@Override
 	public List<WaterSite> loadNewWaterSites(Date stime) {
 
-		List<WaterSite> sites=new ArrayList<WaterSite>();
+		List<WaterSite> wSites = new ArrayList<WaterSite>();
 
-		WaterSite site=new WaterSite();
-		site.setLatitude(323232323.323232);
-		site.setLongitude(898989.43443);
-		site.setSiteId("njj0999");
-		site.setSiteName("和平南路");
+		ISiteManager siteMgr = (ISiteManager) BeanFactory.getBean(ISiteManager.class);
 
-		sites.add(site);
-		return sites;
+		List<MapSite> sites = siteMgr.getSites(stime, TYPE_WATER);
+
+		for (MapSite site : sites) {
+			wSites.add(convertS2W(site));
+		}
+		return wSites;
 	}
 
 	@Override
@@ -44,6 +53,93 @@ public class SiteManagerImpl implements SiteManager {
 
 	}
 
+	public static WaterSite convertS2W(MapSite site) {
+		WaterSite wSite = new WaterSite();
+		wSite.setLatitude(site.getLatitude());
+		wSite.setLongitude(site.getLongitude());
+		wSite.setSiteId(site.getSiteId());
+		wSite.setSiteName(site.getSiteName());
+		wSite.setImageURL(IMGURL);
+		wSite.setImageNUM(site.getImageNUM());
+		wSite.setDocNUM(site.getDocNUM());
+		wSite.setDocURL(DOCURL);
 
+		wSite.setUsable(str2boolean(site.getField0()));// 是否可用
+
+		wSite.setWuShui(str2boolean(site.getField1()));// 无水
+		wSite.setLouShui(str2boolean(site.getField2()));// 漏水
+		wSite.setYalibuzu(str2boolean(site.getField3()));// 压力不足
+		wSite.setXiushi(str2boolean(site.getField4()));// 锈蚀
+		wSite.setMaiya(str2boolean(site.getField5()));// 埋压
+		wSite.setQuanzhan(str2boolean(site.getField6()));// 圈占
+		wSite.setHuihuai(str2boolean(site.getField7()));// 毁坏
+		wSite.setTajing(str2boolean(site.getField8()));// 塌井
+
+		wSite.setBroke(str2boolean(site.getField9()));// 部件损坏
+
+		wSite.setJinggai(str2boolean(site.getField10()));// 井盖
+		wSite.setChushuigai(str2boolean(site.getField11()));// 出水盖
+		wSite.setDinggai(str2boolean(site.getField12()));// 顶盖
+		wSite.setZhafa(str2boolean(site.getField13()));// 闸阀
+		wSite.setChushuikou(str2boolean(site.getField14()));// 出水口
+		wSite.setFangshuifa(str2boolean(site.getField15()));// 放水阀
+		wSite.setShangshuifa(str2boolean(site.getField16()));// 上水阀
+
+		wSite.setComments(site.getField17());// 备注
+		return wSite;
+	}
+
+	public static MapSite convertW2S(WaterSite wSite) {
+
+		MapSite site = new MapSite();
+
+		site.setLatitude(wSite.getLatitude());
+		site.setLongitude(wSite.getLongitude());
+		site.setSiteId(wSite.getSiteId());
+		site.setSiteName(wSite.getSiteName());
+		site.setImageURL(wSite.getImageURL());
+		site.setImageNUM(wSite.getImageNUM());
+		site.setDocNUM(wSite.getDocNUM());
+		site.setDocURL(wSite.getDocURL());
+		site.setSiteType(TYPE_WATER);
+
+		site.setField0(boolean2str(wSite.isUsable()));// 是否可用
+
+		site.setField1(boolean2str(wSite.isWuShui()));// 无水
+		site.setField2(boolean2str(wSite.isLouShui()));// 漏水
+		site.setField3(boolean2str(wSite.isYalibuzu()));// 压力不足
+		site.setField4(boolean2str(wSite.isXiushi()));// 锈蚀
+		site.setField5(boolean2str(wSite.isMaiya()));// 埋压
+		site.setField6(boolean2str(wSite.isQuanzhan()));// 圈占
+		site.setField7(boolean2str(wSite.isHuihuai()));// 毁坏
+		site.setField8(boolean2str(wSite.isTajing()));// 塌井
+
+		site.setField9(boolean2str(wSite.isBroke()));// 部件损坏
+
+		site.setField10(boolean2str(wSite.isJinggai()));// 井盖
+		site.setField11(boolean2str(wSite.isChushuigai()));// 出水盖
+		site.setField12(boolean2str(wSite.isDinggai()));// 顶盖
+		site.setField13(boolean2str(wSite.isZhafa()));// 闸阀
+		site.setField14(boolean2str(wSite.isChushuikou()));// 出水口
+		site.setField15(boolean2str(wSite.isFangshuifa()));// 放水阀
+		site.setField16(boolean2str(wSite.isShangshuifa()));// 上水阀
+
+		site.setField17(wSite.getComments());// 备注
+		return site;
+	}
+
+	public static boolean str2boolean(String s) {
+		if (s == null)
+			return false;
+		else
+			return "1".equals(s);
+	}
+
+	public static String boolean2str(boolean b) {
+		if (!b)
+			return null;
+		else
+			return "1";
+	}
 
 }
